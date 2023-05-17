2d3e9338e0734d9ca149adf387a1519257e9ba2a
Valera <iarchangel@bk.ru>
2023-05-17 14:41:49 +0300

    Переработка сайта

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
  // Проигрывание видео при наведении
   const videos = document.querySelectorAll('video');
 
+
   videos.forEach(video => {
     video.addEventListener('mouseover', () => {
       video.play();
@@ -46,8 +47,52 @@ buttons.forEach((button) => {
   });
 });
 
+const buttonz = document.querySelectorAll(".select2");
+const blockz = document.querySelectorAll(".projects__block2");
+
+buttonz.forEach((button) => {
+  button.addEventListener("click", () => {
+
+    // Удаляем активный класс со всех кнопок
+    buttonz.forEach((button) => {
+      button.classList.remove("select__active");
+      button.querySelector('h2').classList.remove('text__active')
+    button.querySelector('p').classList.remove('text__active')
+    });
+    
+    
+
+    // Добавляем активный класс к нажатой кнопке
+    button.classList.add("select__active");
+    button.querySelector('h2').classList.add('text__active')
+    button.querySelector('p').classList.add('text__active')
+
+    // Скрываем все блоки
+    blockz.forEach((block) => {
+      block.classList.remove('video__active')
+    });
+
+    // Показываем блок, связанный с нажатой кнопкой
+    const target = button.dataset.target;
+    const block = document.getElementById(target);
+    block.classList.add('video__active')
+  });
+});
+
 // Меняем титл в блоке "Проекты" при нажатии на блоки
 function changeTitle(text){
     let title = document.getElementById('changeme')
     title.innerHTML = text
 }
+
+// Ставим тэг автоплей при разрешении <550
+
+const vidos = document.querySelectorAll('video');
+const viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+
+if (viewportWidth < 550) {
+  vidos.forEach(video => {
+    video.setAttribute('autoplay', true);
+  });
+}
+
